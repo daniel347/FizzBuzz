24d288db08d186602c0579f7b8cbc2506b750994
daniel347 <daniel.siromani@softwire.com>
2020-07-06 16:37:07 +0100

    Part 2

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace FizzBuzz
 {
@@ -6,9 +7,11 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int number = 1; number <= 100; ++number)
+            int max_value = 300;
+            for (int number = 1; number <= max_value; ++number)
             {
                 string output = "";
+                
                 if (number % 3 == 0)
                 {
                     output += "Fizz";
@@ -30,11 +33,40 @@ namespace FizzBuzz
                     output = "Bong";
                 }
 
+                if (number % 13 == 0)
+                {
+                    string[] beforeAndAfterB = output.Split('B', 2);
+                    if (beforeAndAfterB.Length == 2)
+                    {
+                        // splits the string and inserts Fezz before the first B
+                        output = beforeAndAfterB[0] + "Fezz" + "B" + beforeAndAfterB[1];
+                    }
+                    else
+                    {
+                        // If no B is found add to the end
+                        output += "Fezz";
+                    }
+                    
+                }
+
+                if (number % 17 == 0)
+                {
+                    if (output.Length >= 8)
+                    {
+                        // if there are at least 2 words in the output,
+                        // swap the order of the first two
+                        output = output.Substring(4, 4) 
+                                 + output.Substring(0, 4)
+                                 + output.Substring(8);
+                    }
+                }
+
                 if (output.Equals(""))
                 {
                     // Only print the number if nothing else is applicable
                     output += number.ToString();
                 }
+                
                 Console.WriteLine(output);
             }
         }
