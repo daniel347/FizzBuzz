68e225c37967cabe8995590eb3a3b10655ee5167
daniel347 <daniel.siromani@softwire.com>
2020-07-06 16:01:02 +0100

    Bang and Bong

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,37 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int number = 1; number <= 100; ++number)
+            {
+                string output = "";
+                if (number % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (number % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+                if (number % 7 == 0)
+                {
+                    output += "Bang";
+                }
+
+                if (number % 11 == 0)
+                {
+                    // If bong is present remove all other outputs
+                    output = "Bong";
+                }
+
+                if (output.Equals(""))
+                {
+                    // Only print the number if nothing else is applicable
+                    output += number.ToString();
+                }
+                Console.WriteLine(output);
+            }
         }
     }
 }
